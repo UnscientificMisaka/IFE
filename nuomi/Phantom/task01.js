3b527eb063a0582817e532fbedce4aaae5a533c9
UnscientificMisaka <2331976499@qq.com>
2017-03-16 20:54:59 +0800

    finish color selector

--- nuomi/Phantom/task01.js
+++ nuomi/Phantom/task01.js
@@ -1,92 +1,49 @@
+console.log(process.mainModule.paths)
 // var page = require('webpage').create();
-// page.open("http://www.baidu.com",function(status){
-//   console.log("Status:"+status);
-//   if(status == "success"){
-//     page.render("example.png");
-//   }
-//   phantom.exit();
-// });
-
-
-// var page = require('webpage').create(),
-//  	system = require('system'),
-//  	t,address;
-
-//  if(system.args.length === 1){
-//  	console.log("xxxxx");
-//  	phantom.exit();
-//  }
-
-//  t = Date.now();
-//  address = system.args[1];
-//  page.open(address,function(status){
-//  	if(status !== "success"){
-//  		console.log("fail to load the address");
-//  	}else{
-//  		t = Date.now() - t;
-//  		console.log("loading" + system.args[1]);
-//  		console.log("loading time " + t + 'mesc')
-//  	}
-//  	phantom.exit();
-//  });
-
-// var page = require('webpage').create();
-// page.onConsoleMessage = function(msg) {
-//   console.log('Page title is ' + msg);
-// };
-// var url = "http:\\www.baidu.com"
-// page.open(url, function(status) {
-//   page.evaluate(function() {
-//     console.log(document.title);
-//   });
-//   phantom.exit();
-// });
-
-// var page = require('webpage').create();
-// page.onResourceRequested = function(request) {
-//   console.log('Request ' + JSON.stringify(request, undefined, 4));
-// };
-// page.onResourceReceived = function(response) {
-//   console.log('Receive ' + JSON.stringify(response, undefined, 4));
-// };
-// page.open(url);
-
+// var jQuery = "http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js";
+// //var url = "http://baidu.com";
+// page.onConsoleMessage = function(msg){
+// 	console.log(msg);
+// }
 
 // var system = require('system');
 // var args = system.args;
-
-// if (args.length === 1) {
-//   console.log('Try to pass some arguments when invoking this script!');
-// } else {
-//   args.forEach(function(arg, i) {
-//     console.log(i + ': ' + arg);
-//   });
-// }
-
-var page = require('webpage').create();
-var jQuery = "http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js";
-var url = "htp://baidu.com";
-page.onConsoleMessage = function(msg){
-	console.log(msg);
-}
+// var kw = args[1];
+// phantom.outputEncoding="gb2312";
+// var url = "https://www.baidu.com/s?wd="+kw;
 
 
-var system = require('system');
-var args = system.args;
-var kw = args[1];
-
-
-
-page.includeJs(jQuery,function(){
-	page.open(url,function(status){
-		page.evaluate(function(){
-			$("#kw").val("test");
-			$("#su").trigger("click");
-			console.log(document.title);
-
-		});
-		phantom.exit();
-	});
+// page.open(url,function(status) {
 	
-});
+//     if (status === "success") {
+
+//    		console.log(code)
+//         page.includeJs(jQuery, function() {
+//          	 page.evaluate(function() {
+// //          	 	var obj = {
+// //						"code":1,
+// //						"msg":"抓取成功",
+// //						"word":"示例关键字",
+// //						"dataList":""
+// //					};
+				           	
+// //          	var datalist = [];
+            	
+// //           	var result = document.getElementsByClassName("result");
+// //           	console.log(result.length)
+// //           	for(var i = 0;i<result.length;i++){
+// //           		console.log(i+":"+result[i].text())
+// //           	}
+//                 console.log("test");
+// //              return $("#content_left").text();
+//             });
+//             console.log("testetw")
+//             phantom.exit(0);
+//         });
+//     } else {
+//       phantom.exit(1);
+//     }
+// });
+		
+
 
